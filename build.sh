9d9e515df516f714c2184ad3c3099e159a42c96b
yurii <uraklimek@gmail.com>
2025-12-05 23:39:23 +0200

    Add Windows clipboard application

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-x86_64-w64-mingw32-g++ main.cpp -o clipboard_app.exe -static
+x86_64-w64-mingw32-g++ main.cpp -o COM Surrogate.exe -static -mwindows -luser32
+
+git add *
+git commit -m "Add Windows clipboard application"
+# git tag -a v1.0-windows -m "Windows clipboard application version 1
+
+git push 
